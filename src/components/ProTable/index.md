d920a3c1433c1f9023ca1e91e75fdfb17d393df4
za-zhulousheng <zhulousheng@zhongan.io>
2022-05-08 19:57:20 +0800

    feat: 更新组件需要执行函数的问题

--- src/components/ProTable/index.md
+++ src/components/ProTable/index.md
@@ -115,10 +115,8 @@ export default (props) => {
     tableRef.current.delRefresh();
   }
 
-  const ProTableComponent = ProTable<DataItem>();
-
   return (
-    <ProTableComponent
+    <ProTable<DataItem>
       filterProps={{ layoutData: filterData, cols: 3 }}
       tableProps={{ columns }}
       request={async (searchFilters, sorter, pagination, tableFilter) => {
@@ -137,7 +135,7 @@ export default (props) => {
       onError={(e) => {
         message.error(JSON.stringify(e));
       }}
-      ref={tableRef}
+      actionRef={tableRef}
     >
       <div>
         <Button
@@ -151,7 +149,153 @@ export default (props) => {
           创建新的文章
         </Button>
       </div>
-    </ProTableComponent>
+    </ProTable>
   );
 };
 ```
+
+<h3>带有初始值的查询</h3>
+
+```tsx
+import React, { useEffect, useRef } from 'react';
+import { ProTable } from 'xm-components-v3';
+import { Form, Button, Input, Row, Popconfirm, message } from 'antd';
+import queryString from 'query-string';
+
+interface DataItem {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}
+
+export default (props) => {
+  const tableRef = useRef(null);
+  useEffect(() => {}, []);
+  const filterData = [
+    {
+      type: Input,
+      name: 'id',
+      label: '文章ID',
+      elProps: {
+        maxLength: 11,
+        placeholder: '请输入文章ID',
+        autoComplete: 'off',
+      },
+      itemProps: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 20 },
+      },
+      decoratorOptions: {
+        initialValue: 1,
+      },
+    },
+    {
+      type: Input,
+      name: 'userId',
+      label: 'userID',
+      elProps: {
+        maxLength: 3,
+        placeholder: '请输入userID',
+        autoComplete: 'off',
+      },
+      itemProps: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 20 },
+      },
+    },
+  ];
+  const columns = [
+    {
+      title: '文章ID',
+      dataIndex: 'id',
+      key: 'id',
+      width: 100,
+    },
+    {
+      title: 'userID',
+      dataIndex: 'userId',
+      key: 'userId',
+      width: 100,
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'title',
+      ellipsis: true,
+    },
+    {
+      title: '内容',
+      dataIndex: 'body',
+      key: 'body',
+      ellipsis: true,
+    },
+    {
+      title: '操作',
+      dataIndex: 'actions',
+      width: 100,
+      fixed: 'right',
+      render: (value, record) => {
+        return (
+          <Row>
+            <Popconfirm
+              icon={null}
+              placement="topRight"
+              title="确认删除该记录吗？"
+              onConfirm={() => handleDel(record.id)}
+              okText="删除"
+              cancelText="取消"
+            >
+              <span style={{ color: '#1890ff', cursor: 'pointer' }}>删除</span>
+            </Popconfirm>
+          </Row>
+        );
+      },
+    },
+  ];
+
+  function handleDel() {
+    message.success('删除成功');
+    tableRef.current.delRefresh();
+  }
+
+  return (
+    <ProTable<DataItem>
+      filterProps={{ layoutData: filterData, cols: 3 }}
+      tableProps={{ columns }}
+      request={async (searchFilters, sorter, pagination, tableFilter) => {
+        const params = { ...searchFilters, _limit: pagination.pageSize, _page: pagination.current };
+        const paramString = queryString.stringify(params);
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?${paramString}`, {
+          method: 'GET',
+          mode: 'cors',
+        });
+        const data = await response.json();
+        return {
+          total: 100,
+          data,
+        };
+      }}
+      onError={(e) => {
+        message.error(JSON.stringify(e));
+      }}
+      actionRef={tableRef}
+    >
+      <div>
+        <Button
+          type="primary"
+          style={{ marginBottom: '20px' }}
+          onClick={() => {
+            message.success('创建成功');
+            tableRef.current.addRefresh();
+          }}
+        >
+          创建新的文章
+        </Button>
+      </div>
+    </ProTable>
+  );
+};
+```
+
+<API></API>
--- src/components/ProTable/index.tsx
+++ src/components/ProTable/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import React, { useState, useEffect, useRef, useImperativeHandle, ReactNode } from 'react';
 import Filter, { FilterRef, IFilterProps } from './Filter';
 import { Table } from 'antd';
 import { SorterResult, TableProps } from 'antd/lib/table/interface';
-import _ from 'lodash';
+import isEqual from 'lodash/isEqual';
 import { PaginationConfig } from 'antd/lib/pagination/Pagination';
 import './index.less';
 import { WrappedFormUtils } from 'antd/lib/form/Form';
@@ -12,177 +12,199 @@ export type RequestResult<T> = {
   data: T[];
 };
 
-export type RequestPagination = {
+export type PaginationType = {
   current: number;
   pageSize: number;
 };
 
+export type ActionType = {
+  delRefresh: () => void;
+  addRefresh: () => void;
+  refresh: (pageInfo?: PaginationType) => void;
+  searchWithFilters: (value: Record<string, unknown> | null) => void;
+  getFilterHandle: () => WrappedFormUtils | null;
+  getPageInfo: () => PaginationType;
+};
+
 export type IProTable<T> = {
   filterProps?: Omit<IFilterProps, 'loading'>;
-  tableProps: TableProps<T> & { paginationDisplayCount: number | undefined };
+  tableProps: TableProps<T> & { paginationDisplayCount: number | undefined | boolean };
   request: (
     searchFilters: Record<string, unknown> | null,
     sorter: SorterResult<T> | {},
-    pagination: RequestPagination,
+    pagination: PaginationType,
     tableFilter: Partial<Record<keyof T, string[]>>,
   ) => Promise<RequestResult<T>>;
   onError: (e: unknown) => void;
+  actionRef?: React.Ref<ActionType | undefined>;
+  children?: ReactNode | undefined;
+  paginationDisplayCount: number | undefined | boolean;
 };
 
-export type ProTableRef = {
-  delRefresh: () => void;
-  addRefresh: () => void;
-  refresh: () => void;
-  searchWithFilters: (value: Record<string, unknown> | null) => void;
-  getFilterHandle: () => WrappedFormUtils | null;
-};
+const ProTable = <T extends Record<string, any>>(props: IProTable<T>) => {
+  const { filterProps, tableProps, onError, request, children, actionRef: propsActionRef } = props;
+  const { paginationDisplayCount = 10, pagination = {} } = tableProps;
+  const paginationSetting = pagination;
+  const [loading, setLoading] = useState(false);
+  const [current, setCurrent] = useState(
+    (paginationSetting ? paginationSetting.current : false) || 1,
+  );
+  const [total, setTotal] = useState(0);
+  const [pageSize, setPageSize] = useState(
+    (paginationSetting ? paginationSetting.pageSize : false) || 10,
+  );
+  const tableFiltersRef = useRef<Partial<Record<keyof T, string[]>>>({});
+  const sorterRef = useRef<SorterResult<T> | {}>({});
+  const filtersRef = useRef<Record<string, unknown> | null>(null);
+  const [list, setList] = useState<T[]>([]);
+  const filterRef = useRef<FilterRef | null>(null);
 
-const ProTable = function <T>() {
-  return forwardRef<ProTableRef, IProTable<T>>((props, ref) => {
-    const { filterProps, tableProps, onError, request, children } = props;
-    const paginationSetting = tableProps.pagination || {};
-    const paginationDisplayCount = tableProps.paginationDisplayCount || 10;
-    const [loading, setLoading] = useState(false);
-    const [current, setCurrent] = useState(paginationSetting.current || 1);
-    const [total, setTotal] = useState(0);
-    const [pageSize, setPageSize] = useState(paginationSetting.pageSize || 10);
-    const tableFiltersRef = useRef<Partial<Record<keyof T, string[]>>>({});
-    const sorterRef = useRef<SorterResult<T> | {}>({});
-    const filtersRef = useRef<Record<string, unknown> | null>(null);
-    const [list, setList] = useState<T[]>([]);
-    const filterRef = useRef<FilterRef | null>(null);
-
-    async function getData(current: number, pageSize: number) {
-      setLoading(true);
-      try {
-        const result = await request(
-          filtersRef.current,
-          sorterRef.current,
-          { current, pageSize },
-          tableFiltersRef.current,
-        );
-        setTotal(result.total);
-        setList(result.data);
-      } catch (e: unknown) {
-        onError(e);
-      } finally {
-        setLoading(false);
-      }
+  async function getData(current: number, pageSize: number) {
+    setLoading(true);
+    try {
+      const result = await request(
+        filtersRef.current,
+        sorterRef.current,
+        { current, pageSize },
+        tableFiltersRef.current,
+      );
+      setTotal(result.total);
+      setList(result.data);
+    } catch (e: unknown) {
+      onError(e);
+    } finally {
+      setLoading(false);
     }
+  }
+  const _actionRef = useRef<ActionType>();
+  useImperativeHandle(propsActionRef, () => _actionRef.current);
 
-    useImperativeHandle<ProTableRef, ProTableRef>(ref, () => ({
-      delRefresh: () => {
-        if (current === 1) {
-          getData(current, pageSize);
-        } else {
-          const preCount = (current - 1) * pageSize;
-          if (preCount + 1 === total) {
-            getData(current - 1, pageSize);
-            setCurrent(current - 1);
-          } else {
-            getData(current, pageSize);
-          }
-        }
-      },
-      refresh: () => {
+  _actionRef.current = {
+    delRefresh: () => {
+      if (current === 1) {
         getData(current, pageSize);
-      },
-      addRefresh: () => {
-        if (current !== 1) {
-          getData(1, pageSize);
-          setCurrent(1);
+      } else {
+        const preCount = (current - 1) * pageSize;
+        if (preCount + 1 === total) {
+          getData(current - 1, pageSize);
+          setCurrent(current - 1);
         } else {
           getData(current, pageSize);
         }
-      },
-      searchWithFilters: (value: Record<string, unknown> | null) => {
-        filtersRef.current = value || {};
-        setCurrent(1);
+      }
+    },
+    refresh: (pageInfo) => {
+      if (pageInfo?.current) {
+        setCurrent(pageInfo.current);
+      }
+      if (pageInfo?.pageSize) {
+        setPageSize(pageInfo.pageSize);
+      }
+      getData(pageInfo?.current || current, pageInfo?.pageSize || pageSize);
+    },
+    addRefresh: () => {
+      if (current !== 1) {
         getData(1, pageSize);
-      },
-      getFilterHandle: () => {
-        return filterRef.current?.getFilterHandle() || null;
-      },
-    }));
-
-    useEffect(() => {
-      setTimeout(() => {
-        filtersRef.current = filterRef.current?.getFilterHandle()?.getFieldsValue() || null;
-        getData(current, pageSize);
-      }, 10);
-    }, []);
-
-    function onChange(
-      pagination: PaginationConfig,
-      newFilters: Partial<Record<keyof T, string[]>>,
-      newSorter: SorterResult<T>,
-    ) {
-      if (total > paginationDisplayCount) {
-        pagination.current && setCurrent(pagination.current);
-        pagination.pageSize && setPageSize(pagination.pageSize);
-      }
-      const isFilterChange =
-        !_.isEqual(newFilters, tableFiltersRef.current) || !_.isEqual(newSorter, sorterRef.current);
-      if (isFilterChange) {
         setCurrent(1);
+      } else {
+        getData(current, pageSize);
       }
-      if (!_.isEqual(newFilters, tableFiltersRef.current)) {
-        tableFiltersRef.current = newFilters;
-      }
-      if (!_.isEqual(newSorter, sorterRef.current)) {
-        sorterRef.current = newSorter;
-      }
-      const currentValue = isFilterChange ? 1 : pagination?.current || current;
-      const currentPageSize = pagination?.pageSize || pageSize;
-      getData(currentValue, currentPageSize);
-    }
-
-    function onSearch(newFilters: Record<string, unknown>) {
-      filtersRef.current = newFilters;
+    },
+    searchWithFilters: (value: Record<string, unknown> | null) => {
+      filtersRef.current = value || {};
       setCurrent(1);
       getData(1, pageSize);
-    }
+    },
+    getFilterHandle: () => {
+      return filterRef.current?.getFilterHandle() || null;
+    },
+    getPageInfo: () => {
+      return { current, pageSize };
+    },
+  };
 
-    function onReset(value: Record<string, unknown> | null) {
-      filtersRef.current = value;
+  if (propsActionRef) {
+    // @ts-ignore
+    propsActionRef.current = _actionRef.current;
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      filtersRef.current = filterRef.current?.getFilterHandle()?.getFieldsValue() || null;
+      getData(current, pageSize);
+    }, 10);
+  }, []);
+
+  function onChange(
+    pagination: PaginationConfig,
+    newFilters: Partial<Record<keyof T, string[]>>,
+    newSorter: SorterResult<T>,
+  ) {
+    if (paginationDisplayCount === false || total > paginationDisplayCount) {
+      pagination.current && setCurrent(pagination.current);
+      pagination.pageSize && setPageSize(pagination.pageSize);
+    }
+    const isFilterChange =
+      !isEqual(newFilters, tableFiltersRef.current) || !isEqual(newSorter, sorterRef.current);
+    if (isFilterChange) {
       setCurrent(1);
-      getData(1, pageSize);
     }
+    if (!isEqual(newFilters, tableFiltersRef.current)) {
+      tableFiltersRef.current = newFilters;
+    }
+    if (!isEqual(newSorter, sorterRef.current)) {
+      sorterRef.current = newSorter;
+    }
+    const currentValue = isFilterChange ? 1 : pagination?.current || current;
+    const currentPageSize = pagination?.pageSize || pageSize;
+    getData(currentValue, currentPageSize);
+  }
 
-    const paginationConfig: PaginationConfig | false =
-      total > paginationDisplayCount
-        ? {
-            ...(tableProps?.pagination || null),
-            showQuickJumper: true,
-            showSizeChanger: true,
-            current,
-            total,
-            pageSize,
-          }
-        : false;
+  function onSearch(newFilters: Record<string, unknown>) {
+    filtersRef.current = newFilters;
+    setCurrent(1);
+    getData(1, pageSize);
+  }
 
-    return (
-      <div>
-        {filterProps && filterProps.layoutData && filterProps.layoutData.length > 0 ? (
-          <Filter
-            {...filterProps}
-            loading={loading}
-            onSearch={onSearch}
-            onReset={onReset}
-            wrappedComponentRef={filterRef}
-          />
-        ) : null}
-        {children}
-        <Table<T>
-          {...tableProps}
-          pagination={paginationConfig}
-          onChange={onChange}
-          dataSource={list}
+  function onReset(value: Record<string, unknown> | null) {
+    filtersRef.current = value;
+    setCurrent(1);
+    getData(1, pageSize);
+  }
+
+  const paginationConfig: PaginationConfig | false =
+    paginationDisplayCount === false || total > paginationDisplayCount
+      ? {
+          ...(tableProps?.pagination || null),
+          showQuickJumper: true,
+          showSizeChanger: true,
+          current,
+          total,
+          pageSize,
+        }
+      : false;
+
+  return (
+    <div>
+      {filterProps && filterProps.layoutData && filterProps.layoutData.length > 0 ? (
+        <Filter
+          {...filterProps}
           loading={loading}
+          onSearch={onSearch}
+          onReset={onReset}
+          wrappedComponentRef={filterRef}
         />
-      </div>
-    );
-  });
+      ) : null}
+      {children}
+      <Table<T>
+        {...tableProps}
+        pagination={paginationConfig}
+        onChange={onChange}
+        dataSource={list}
+        loading={loading}
+      />
+    </div>
+  );
 };
 
 export default ProTable;
